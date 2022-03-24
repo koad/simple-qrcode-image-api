7a6c620a3478418939d02a135a57788c36b99ccd
koad <koad@ecoincore.com>
2022-03-23 20:34:13 -0400

    made qr a bit smaller, so the new footer would fit.

--- client/logic.js
+++ client/logic.js
@@ -9,10 +9,10 @@ Template.app.helpers({
 
   // this is likely shit, TODO: find a way to make this better
   size() {
-    let h = document.documentElement.clientHeight-84;
+    let h = document.documentElement.clientHeight-104;
     let w = document.documentElement.clientWidth-96;
     if (h > w) return w; // if the window is longer than it is wide then return this value now
-    if(h < 256) h = 256; // Make sure the QR code at least this big
+    if(h < 128) h = 128; // Make sure the QR code at least this big
     span = (w - h) / 2;  // create some padding to the left hand side of the QR code to center it
     return h
   },
