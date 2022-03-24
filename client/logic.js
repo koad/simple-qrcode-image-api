ea8c57c02c6b859f6224d71fde20a3d4d225c0db
koad <koad@ecoincore.com>
2022-03-24 14:42:21 -0400

    cleanup

--- client/logic.js
+++ client/logic.js
@@ -1,25 +1,32 @@
 import { Template } from 'meteor/templating';
 
-var span = 0;
+Router.route('/', function () {
+  this.render('welcome');
+});
+
+Router.route('/:_qrcode_content', function () { 
+  this.render('app');
+});
 
 Template.app.helpers({
   url() {
-    return location.pathname.substr(1);
+    if(!Router.current().params._qrcode_content) return false;
+    return Router.current().params._qrcode_content;
   },
-
-  // this is likely shit, TODO: find a way to make this better
   size() {
     let h = document.documentElement.clientHeight-104;
     let w = document.documentElement.clientWidth-96;
-    if (h > w) return w; // if the window is longer than it is wide then return this value now
+    if (h > w) h = w; // if the window is longer than it is wide then return this value now
+
+
     if(h < 128) h = 128; // Make sure the QR code at least this big
-    span = (w - h) / 2;  // create some padding to the left hand side of the QR code to center it
+
+    let span = (w - h) / 2;  // create some padding to the left hand side of the QR code to center it
+
+
+  h = h - 80;
+    // Since the element we want to adjust
+    // Meteor.setTimeout(()=>{ document.getElementById("codeBox").style.paddingLeft = span+"px" }, 3);
     return h
   },
 });
-
-// this is likely shit too, TODO: find a way to make this unnecessary
-Template.app.rendered = function() {
-  // Once the page is rendered, adjust the padding to center the QR code.
-  document.getElementById("codeBox").style.paddingLeft = span+"px";
-}
